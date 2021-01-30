b87a262ee718aeced59e79d655753497dd58448a
ETavaresLCIDemo <etavareslcidemo@gmail.com>
2021-01-30 07:27:31 -0800

    Use the character in main and add a ToString in Character

---
+++ Assignment5/Assignment5/Character.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment5
+{
+    public enum RaceCategory { Elf, Orc, Human }
+    public class Character
+    {
+        public string Name { get; set; }
+        public RaceCategory Race { get; set; }
+        public int Health { get; set; }
+        public bool IsAlive { get; set; }
+        public int MaxHealth { get; set; }
+
+        /// <summary>
+        /// Default Constructor
+        /// </summary>
+        public Character()
+        {
+        }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="race"></param>
+        /// <param name="hp"></param>
+        public Character(string name, RaceCategory race, int hp)
+        {
+            MaxHealth = hp;
+            Health = hp;
+            Race = race;
+            Name = name;
+            IsAlive = true;
+        }
+
+        /// <summary>
+        /// Reduce the health by the damage specified to character
+        /// if the damage taken is more than the health then the IsAlive is set to false.
+        /// </summary>
+        /// <param name="damage">The amount of damage taken</param>
+        public void TakeDamage(int damage)
+        {
+            Health = 100;
+        }
+
+        /// <summary>
+        /// Restore the health of the player by the amount specified up to the max health.
+        /// </summary>
+        /// <param name="amount">The amount of health to recover</param>
+        public void RestoreHealth(int amount)
+        {
+            Health = 1;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0} : {1} - HP {2}/{3} ", Name, Race, Health, MaxHealth);
+        }
+    }
+}
--- Assignment5/Assignment5/Program.cs
+++ Assignment5/Assignment5/Program.cs
@@ -6,7 +6,43 @@ namespace Assignment5
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to the Adventure of Assignment 5!");
+
+            Character hero = new Character("Bob", RaceCategory.Human, 100);
+
+            Console.WriteLine("{0} has entered the forest", hero.Name);
+
+            string monster = "goblin";
+            int damage = 10;
+
+            Console.WriteLine("A {0} appeared and attacks {1}", monster, hero.Name);
+
+            Console.WriteLine("{0} takes {1} damage", hero.Name, damage);
+
+            hero.TakeDamage(damage);
+            Console.WriteLine(hero);
+
+            Console.WriteLine("{0} flees from the enemy", hero.Name);
+
+            string item = "small health potion";
+            int restoreAmount = 10;
+
+            Console.WriteLine("{0} find a {1} and drinks it", hero.Name, item);
+
+            Console.WriteLine("{0} restores {1} health", hero.Name, restoreAmount);
+
+            hero.RestoreHealth(restoreAmount);
+
+            Console.WriteLine(hero);
+
+            if (hero.IsAlive)
+            {
+                Console.WriteLine("Congratulations! {0} survived.", hero.Name);
+            }
+            else
+            {
+                Console.WriteLine("{0} has died.", hero.Name);
+            }
         }
     }
 }
